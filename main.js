4d16a49ebfe7c8066cbea4293c8dfc918ccdb477
SujithRagavendra <88975772+SujithRagavendra@users.noreply.github.com>
2022-04-28 19:55:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(340 , 340)
+    canvas.center()
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save("downloadShot.jpg")
+}
